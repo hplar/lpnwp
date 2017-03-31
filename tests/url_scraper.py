5707ca8a9a63293e32e1fe12f7363c3dd6431f96
Ralph Lamballais <ralph@basenet.nl>
2017-03-31 12:09:05 +0200

    small prompt and output changes

--- tests/url_scraper.py
+++ tests/url_scraper.py
@@ -3,17 +3,17 @@
 from urllib.request import urlopen
 
 
-url = input(str("URL: "))
+url = input(str("[?] URL: "))
 response = urlopen("http://{}".format(url))
 
 target_file = "./downloads/{}.html".format(url)
 
 with open(target_file, 'w') as f:
-    print("opening file and starting write")
+    print("[*] opening file and starting write")
     line = response.readline().decode()
     while True:
         f.write(line)
         line = response.readline().decode()
         if line == "":
-            print("no more lines -> exit.")
+            print("[*] no more lines -> exit.")
             exit()
